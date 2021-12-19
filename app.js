37663490cc810394429d4441d37713e1335eda1b
Jamesxtmp <gen22xtmp@gmail.com>
2021-12-19 14:59:42 -0600

    31-Representacion declarativa

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const app = Vue.createApp({
-    template: `
-        <h1>Hola Mundo</h1>
-        <p>Desde vue</p>
-    
-    `
+    // template: `
+    //     <h1> Hola Mundo </h1>
+    //     <p> Desde Vue </p>
+    // `
 })
 app.mount('#AppVue')
