94c4bce71e605496910ce25a850d506c1bc5db32
Jamesxtmp <gen22xtmp@gmail.com>
2021-12-19 15:15:14 -0600

    32. Estado del componente - Data

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ const app = Vue.createApp({
     //     <h1> Hola Mundo </h1>
     //     <p> Desde Vue </p>
     // `
+
+    data(){
+        return{
+            message: 'Hola mundo',
+            title: 'La programacion',
+            autor: 'Jaime'
+        }
+    }
 })
 app.mount('#AppVue')
